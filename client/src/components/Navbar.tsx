5f012f31adb170bfdd71ab0bca11a6285df914ef
jvjmarinello <jeremy.marinello@gmail.com>
2024-12-20 04:28:38 -0700

    setup component library

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import { Button } from "@/components/ui/button"
 
 export default function Navbar() {
   return (
-    <div>
+    <div style={{display: 'flex', gap: '20px'}}>
       <Link to="/">Home</Link>
       <Link to="/signup">Sign Up</Link>
       <Link to="/data">Data</Link>
       <Link to="/admin">Administration</Link>
+      <Button>Sign Up</Button>
     </div>
   )
 };
