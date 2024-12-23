e060649a4be92ecfc4d67b512f02c7f1f94e3471
jvjmarinello <jeremy.marinello@gmail.com>
2024-12-23 10:43:49 -0700

    updated package.json

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
